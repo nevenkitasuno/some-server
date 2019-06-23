7f2b4b3fc11ff52d874c454902b1441bf8ace6c7
nevenkitasuno <38082848+nevenkitasuno@users.noreply.github.com>
2019-06-23 07:32:43 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,52 +1,43 @@
-#pragma comment(lib,"ws2_32.lib")
-#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <tchar.h>
+#include "fazl.h"
 
-#pragma warning(disable: 4996)
-
-int zapros(SOCKET);
-
-int main(int argc, char* argv[]) {
-    //WSAStartup
+int main() {
     WSAData wsaData;
-    WORD DDLVersion = MAKEWORD(2, 1);
-    if(WSAStartup(DDLVersion, &wsaData) != 0) {
+    WORD DLLVersion = MAKEWORD(2, 1);
+    if(WSAStartup(DLLVersion, &wsaData) != 0) {
         std::cout << "Error #C1: winsock didn't loaded" << std::endl;
         exit(1);
-    } //Подгружаем WinSock
+    }
 
-    //Информация об адресе сервера
-    SOCKADDR_IN addr;
-    int sizeOfAddr = sizeof(addr);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addr.sin_port = htons(1111);
-    addr.sin_family = AF_INET;
+    SOCKADDR_IN server_addr;
+    int sizeOf_sAddr = sizeof(server_addr);
+    InetPton(AF_INET, _T("127.0.0.1"), &server_addr.sin_addr.s_addr);
+    server_addr.sin_port = htons(1111);
+    server_addr.sin_family = AF_INET;
 
-    SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL); //Сокет для соединения с сервером
-    if(connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) { //Попытка соединения. Параметры как у слушающего accept'а
+    SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+    if(connect(Connection, (SOCKADDR*)&server_addr, sizeOf_sAddr) != 0) {
         std::cout << "Error #C2: connection failed" << std::endl;
         std::cout << WSAGetLastError() << std::endl;
-        exit(1);
+        exit(2);
     }
     std::cout << "Connected to server" << std::endl;
 
-    //char msg[256];
-    //recv(Connection, msg, sizeof(msg), NULL); //Принимаем сообщение
-    //std::cout << msg << std::endl;
-    char *msg = "give_all_ips";
-    int msg_size = sizeof(msg);
-    send(Connection, (char*)msg_size, sizeof(msg_size), NULL);
-    send(Connection, msg, msg_size, NULL);
-    delete[] msg;
+    char cmd[CMD_LEN];
+    long lmsg;
+    char msg[256];
+    strcpy(cmd, "allip");
+    send(Connection, cmd, sizeof(cmd), NULL);
+    recv(Connection, (char*)&lmsg, sizeof(lmsg), NULL);
 
-    recv(Connection, (char*)&msg_size, sizeof(msg_size), NULL);
-    msg = new char[msg_size + 1];
-    msg[msg_size] = '\0';
-    recv(Connection, msg, msg_size, NULL);
+    for(long i = lmsg; i > 0; i--) {
+        recv(Connection, msg, sizeof(msg), NULL);
+        std::cout << msg << std::endl;
+    }
 
-    std::cout << msg;
-    delete[] msg;
-
-    system("pause");
     return 0;
 }
